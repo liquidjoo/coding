b45857f7bea2979ede4849fedb71db504c87f24f
liquidjoo <liquidjoo@gmail.com>
2018-11-06 22:41:24 +0900

    2292

--- p/2292.py
+++ p/2292.py
@@ -1,12 +1,55 @@
-number = int(input())
+"""
+정육면체 6
+[1], [2,3,4,5,6,7], [8,9,10,11,12,13,14,15,16,17,18,19], [20~ 37 ],
+1       6             12
 
-if number == 1:
-    print(1)
-    exit()
+1, 2, 8, 20, 38, 62
+
+  1 6 12 18 24
+
+
+1 6n
+
+
+
+
+
+
+8
+
+
+
+1  2  3   4  5   6
+
+
+58 // 6
+
+"""
+a = input()
+a = int(a)
 index = 0
+number = 1
 while True:
-    if 1+(6*index) < number:
-        index += 1
-    else:
-        print(index+1)
+
+    if a < number:
+        print(index)
         break
+    else:
+        if number == 1:
+            number += 1
+        else:
+            number = number + (index * 6)
+        index += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
